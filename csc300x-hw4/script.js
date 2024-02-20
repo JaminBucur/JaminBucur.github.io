2826f060b8032bbd38b6ec34c6a4b077cdb06f04
JaminBucur <jcbucur@uncg.edu>
2024-02-19 20:46:02 -0500

    Finished

---
+++ csc300x-hw4/script.js
@@ -0,0 +1,61 @@
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+
+function playerThrow(choice) {
+    if (document.querySelector('#player-throw img:not(.disabled)')) {
+        let images = document.querySelectorAll('#player-throw img');
+        for (let img of images) {
+            img.classList.remove('selected');
+            img.classList.add('disabled');
+        }
+
+        let computerImage = document.getElementById('computer-choice');
+        computerImage.src = 'question-mark.png';
+        document.querySelector('#player-throw img[src="' + choice + '.png"]').classList.add('selected');
+        document.getElementById('outcome').innerText = 'Thinking...';
+
+        setTimeout(function () {
+            let computerChoice = ['rock', 'paper', 'scissors'][Math.floor(Math.random() * 3)];
+            let computerImage = document.getElementById('computer-choice');
+            computerImage.src = computerChoice + '.png';
+            computerImage.classList.add('computer-selected');
+
+            if (choice === computerChoice) {
+                tieScore++;
+                document.getElementById('outcome').innerText = 'It\'s a tie!';
+            } else if (
+                (choice === 'rock' && computerChoice === 'scissors') ||
+                (choice === 'paper' && computerChoice === 'rock') ||
+                (choice === 'scissors' && computerChoice === 'paper')
+            ) {
+                playerScore++;
+                document.getElementById('outcome').innerText = 'You win!';
+            } else {
+                computerScore++;
+                document.getElementById('outcome').innerText = 'Computer wins!';
+            }
+
+            document.getElementById('score').innerText = 'Player: ' + playerScore + ', Computer: ' + computerScore + ', Ties: ' + tieScore;
+
+            for (let img of images) {
+                img.classList.remove('selected', 'disabled');
+            }
+            computerImage.classList.remove('computer-selected');
+        }, 3000);
+    }
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    tieScore = 0;
+    document.getElementById('computer-choice').src = 'question-mark.png';
+    document.getElementById('outcome').innerText = '';
+    document.getElementById('score').innerText = '';
+
+    let images = document.querySelectorAll('#player-throw img');
+    for (let img of images) {
+        img.classList.remove('disabled');
+    }
+}
